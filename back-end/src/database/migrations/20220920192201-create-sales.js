e99ee753670e7b66bd4b87c296f29af8f0f9f389
thiago <ts_muniz@outlook.com>
2022-09-20 16:35:07 -0300

    feat: adiciona a migration de vendas

---
+++ back-end/src/database/migrations/20220920192201-create-sales.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        fields: "user_id",
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          // Informa a tabela da referência da associação
+          model: 'users',
+          // Informa a coluna da referência que é a chave correspondente
+          key: 'id',
+        },
+      },
+      sellerId: {
+        type: Sequelize.INTEGER,
+        fields: "seller_id",
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          // Informa a tabela da referência da associação
+          model: 'users',
+          // Informa a coluna da referência que é a chave correspondente
+          key: 'id',
+        },
+      },
+      totalPrice: {
+        type: Sequelize.DECIMAL(9,2),
+        allowNull: false,
+        fields: "total_price"
+      },
+      deliveryAddress: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        fields: "delivery_address"
+      },
+      deliveryNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        fields: "delivery_number"
+      },
+      saleDate: {
+        type: Sequelize.DATETIME,
+        allowNull: false,
+        fields: "sale_date"
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('sales');
+  }
+};
