0034ec7629c43d7210d84b396e75d50a04c59ef3
AllineFranciely <alline_franciely@hotmail.com>
2022-09-23 19:29:34 -0300

    criação da paǵina de products

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Cadastro from './pages/Cadastro/Cadastro';
+import Products from './pages/Products/Products';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={ <Login /> } />
-      <Route exact path="/" element={ <Login /> } />
+      <Route exact path="/" element={ <Navigate to="/login" replace /> } />
       <Route path="/register" element={ <Cadastro /> } />
+      <Route path="/customer/products" element={ <Products /> } />
     </Routes>
   );
 }
