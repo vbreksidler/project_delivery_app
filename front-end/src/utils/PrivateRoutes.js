69e39a19153f955ee327a5e6583e76739c61591d
thiago <ts_muniz@outlook.com>
2022-09-26 08:04:23 -0300

    Documentation: adiciona comentario no context

--- front-end/src/utils/PrivateRoutes.js
+++ front-end/src/utils/PrivateRoutes.js
@@ -3,7 +3,7 @@ import { Outlet, Navigate } from 'react-router-dom';
 import React from 'react';
 
 export default function PrivateRoutes() {
-  const auth = { token: true };
+  const auth = { token: true }; // esta feito assim pois ainda nao alinhei com resto do pessoal sobre como vamos enviar isto pra ca no formulario de login
 
   return auth.token ? <Outlet /> : <Navigate to="/" />;
 }
