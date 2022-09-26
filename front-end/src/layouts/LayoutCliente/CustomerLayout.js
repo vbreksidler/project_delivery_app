1996637855e5a863f6882b5b384082ba725cfea4
thiago <ts_muniz@outlook.com>
2022-09-26 13:18:38 -0300

    feat: adiciona card de status de pedidos

--- front-end/src/layouts/LayoutCliente/CustomerLayout.js
+++ front-end/src/layouts/LayoutCliente/CustomerLayout.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../../componentes/NavBar/NavBar';
+import NavBarLink from '../../componentes/NavBarLink/NavBarLink';
 
 export default function CustomerLayout() {
   return (
     <>
-      <NavBar />
+      <NavBar>
+        <NavBarLink name="Produtos" path="/customer/products" />
+        <NavBarLink name="Meus Pedidos" path="/customer/products" />
+      </NavBar>
       <Outlet />
     </>
   );
