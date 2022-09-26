11d445783e7eefe9d76bfb2302e2e489e5b82494
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-09-26 20:46:04 -0300

    refactor retira o testid do ButtonLogout

--- front-end/src/componentes/NavBar/NavBar.js
+++ front-end/src/componentes/NavBar/NavBar.js
@@ -6,12 +6,10 @@ function NavBar({ userName, children }) {
   return (
     <div>
       { children }
-      <p dataTestId="customer_products__element-navbar-user-full-name">
+      <p data-testid="customer_products__element-navbar-user-full-name">
         { userName }
       </p>
-      <ButtonLogout
-        dataTestId="customer_products__element-navbar-link-logout"
-      />
+      <ButtonLogout />
     </div>
   );
 }
