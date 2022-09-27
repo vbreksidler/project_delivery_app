ea7994e568599046a0ebd9ee45398e0ab933b69f
Victor Bariatto Reksidler <vbreksidler@gmail.com>
2022-09-27 16:50:00 -0300

    fix: images

--- back-end/src/utils/token.js
+++ back-end/src/utils/token.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
-const secret = 'batatinha123';
+const secretKey = fs.readFileSync('jwt.evaluation.key', { encoding: 'utf8', flag: 'r' });
+const secret = secretKey.split('=')[1] || 'batatinha123';
 
 async function createToken(user) {
     const { email, password, role } = user;
