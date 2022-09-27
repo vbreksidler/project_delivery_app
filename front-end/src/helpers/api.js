b9e89868f8d6646fe1cba2b7178909921d52d4db
thiago <ts_muniz@outlook.com>
2022-09-27 16:06:11 -0300

    feat: adiciona header de validacao no front

--- front-end/src/helpers/api.js
+++ front-end/src/helpers/api.js
@@ -4,4 +4,14 @@ const api = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
+api.interceptors.request.use(async (config) => {
+  const user = localStorage.getItem('user');
+
+  if (token) {
+    config.headers.Authorization = user.token;
+  }
+
+  return config;
+});
+
 export default api;
