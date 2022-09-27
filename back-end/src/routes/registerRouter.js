de0d918af714d42c3193874de1767e20eb49fa31
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-09-27 13:30:44 -0300

    feat: adiciona cadastro de usuário para admin

--- back-end/src/routes/registerRouter.js
+++ back-end/src/routes/registerRouter.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 
 const userController = require('../controllers/userControler');
+const authenticate = require('../middlewares/authMiddleware');
 
 const registerRoute = Router();
 
-registerRoute.post('/', userController.create);
+registerRoute.post('/', userController.createUserCustomer);
+
+registerRoute.post('/admin', authenticate, userController.createUser);
 
 module.exports = registerRoute;
