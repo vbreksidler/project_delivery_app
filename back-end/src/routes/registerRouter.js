51caf2a751b47264ce4cf170416fbd4e614ea5a8
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-09-27 19:38:21 -0300

    fix: altera ordem das rotas

--- back-end/src/routes/registerRouter.js
+++ back-end/src/routes/registerRouter.js
@@ -5,8 +5,8 @@ const authenticate = require('../middlewares/authMiddleware');
 
 const registerRoute = Router();
 
-registerRoute.post('/', userController.createUserCustomer);
-
 registerRoute.post('/admin', authenticate, userController.createUser);
+
+registerRoute.post('/', userController.createUserCustomer);
 
 module.exports = registerRoute;
