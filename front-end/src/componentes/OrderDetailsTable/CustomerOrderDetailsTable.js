bf0da950db62ba2ed19a18c792e3f325e1abe332
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-09-30 18:48:14 -0300

    refactor: tabelas

--- front-end/src/componentes/OrderDetailsTable/CustomerOrderDetailsTable.js
+++ front-end/src/componentes/OrderDetailsTable/CustomerOrderDetailsTable.js
@@ -1,48 +1,8 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import OrderDetailsTable from './OrderDetailsTable';
 
-export default function CustomerOrderDetailsTable(
-  { position, description, quantity, unityValue, subTotal },
-) {
+export default function CustomerOrderDetailsTable(props) {
   return (
-
-    <tr>
-      <td
-        data-testid={ `customer_order_details__element-order
-          -table-item-number-${position}` }
-      >
-        {position}
-      </td>
-      <td
-        data-testid={ `customer_order_details__element-order-table-name-${description}` }
-      >
-        {description}
-      </td>
-      <td
-        data-testid={ `customer_order_details__element-order-table-quantity-${quantity}` }
-      >
-        {quantity}
-      </td>
-      <td
-        data-testid={ `customer_order_details__element-order-table
-          -unit-price-${unityValue}` }
-      >
-        {unityValue}
-      </td>
-      <td
-        data-testid={ `customer_order_details__element-order-table
-          -sub-total-${subTotal}` }
-      >
-        {subTotal}
-      </td>
-    </tr>
+    <OrderDetailsTable userType="customer" { ...props } />
   );
 }
-
-CustomerOrderDetailsTable.propTypes = {
-  description: PropTypes.number.isRequired,
-  position: PropTypes.string.isRequired,
-  quantity: PropTypes.string.isRequired,
-  subTotal: PropTypes.string.isRequired,
-  unityValue: PropTypes.string.isRequired,
-};
--- front-end/src/componentes/OrderDetailsTable/SellerOrderDetailsTable.js
+++ front-end/src/componentes/OrderDetailsTable/SellerOrderDetailsTable.js
@@ -1,49 +1,8 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import OrderDetailsTable from './OrderDetailsTable';
 
-export default function SellerOrderDetailsTable(
-  { position, description, quantity, unityValue, subTotal },
-) {
+export default function SellerOrderDetailsTable(props) {
   return (
-
-    <tr>
-      <td
-        data-testid={ `seller_order_details__element-order-
-          table-item-number-${position}` }
-      >
-        {position}
-      </td>
-      <td
-        data-testid={ `seller_order_details__element
-        -order-table-name-${description}` }
-      >
-        {description}
-      </td>
-      <td
-        data-testid={ `seller_order_details__element-order-table-quantity-${quantity}` }
-      >
-        {quantity}
-      </td>
-      <td
-        data-testid={ `seller_order_details__element-
-          order-table-unit-price-${unityValue}` }
-      >
-        {unityValue}
-      </td>
-      <td
-        data-testid={ `seller_order_details__element-order-
-          table-sub-total-${subTotal}` }
-      >
-        {subTotal}
-      </td>
-    </tr>
+    <OrderDetailsTable userType="seller" { ...props } />
   );
 }
-
-SellerOrderDetailsTable.propTypes = {
-  description: PropTypes.number.isRequired,
-  position: PropTypes.string.isRequired,
-  quantity: PropTypes.string.isRequired,
-  subTotal: PropTypes.string.isRequired,
-  unityValue: PropTypes.string.isRequired,
-};
