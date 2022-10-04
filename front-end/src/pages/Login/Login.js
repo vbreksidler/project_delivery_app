44d5e9e443d711b0b3c0fab736f7e41a9470f65f
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-10-04 15:15:22 -0300

    adiciona logo na pagina de login

--- front-end/src/pages/Login/Login.js
+++ front-end/src/pages/Login/Login.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import FormLogin from '../../componentes/Forms/FormLogin';
+import Logo from '../../Logo.png';
+import './Login.css';
 
 function Login() {
   return (
-    <FormLogin />
+    <div className="LoginPage">
+      <img src={ Logo } alt="logo" className="Logo" />
+      <div className="Divisor" />
+      <FormLogin />
+    </div>
   );
 }
 
